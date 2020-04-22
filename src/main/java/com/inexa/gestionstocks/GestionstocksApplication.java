d2dd63e83833ee4fd6b84925f1460d535aa8084c
Diarra <diarramamadou73@gmail.com>
2020-04-22 10:38:29 +0000

    initialise project

---
+++ src/main/java/com/inexa/gestionstocks/GestionstocksApplication.java
@@ -0,0 +1,13 @@
+package com.inexa.gestionstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionstocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionstocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inexa/gestionstocks/GestionstocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inexa.gestionstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionstocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
