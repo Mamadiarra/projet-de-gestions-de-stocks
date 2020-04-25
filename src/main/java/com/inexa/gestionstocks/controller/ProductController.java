3b7ba29fc93029a415deb91c58cda22d83a7645e
Diarra <diarramamadou73@gmail.com>
2020-04-25 17:27:18 +0000

    Formation2é

---
+++ src/main/java/com/inexa/gestionstocks/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.inexa.gestionstocks.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProductController {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/")
+    public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        log.info("Call home page");
+        return "home";
+    }
+
+    @GetMapping("/toto")
+    public String myProduct(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        log.info("Call product page");
+        return "myproduct";
+    }
+
+}
